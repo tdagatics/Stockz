06eb20d98f7741dcfb64fe803daca55d50f4778f
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-03 13:32:44 -0700

    First commit of Stockz code

--- Stockz/main.m
+++ Stockz/main.m
@@ -14,8 +14,21 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSMutableArray *stocks = [[NSMutableArray alloc] init];
         
+        NSMutableDictionary *stock;
+        
+        stock = [NSMutableDictionary dictionary];
+        [stock setObject:@"AAPL" forKey:@"symbol"];
+        [stock setObject:[NSNumber numberWithInt:200] forKey:@"shares"];
+        [stocks addObject:stock];
+        
+        stock = [NSMutableDictionary dictionary];
+        [stock setObject:@"GOOG" forKey:@"symbol"];
+        [stock setObject:[NSNumber numberWithInt:1000] forKey:@"shares"];
+        [stocks addObject:stock];
+        
+        [stocks writeToFile:@"/tmp/stocks.plist" atomically:YES];
     }
     return 0;
 }
